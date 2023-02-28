3ddab8888b942c42e312c9257f4475c4526b12fa
micky ni <mickyn0415@hstat.org>
2023-02-28 18:24:19 +0000

    Tue, Feb 28, 2023, 6:24 PM +00:00

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "mickyn0415"; // update the part BETWEEN the "quotes"
+let userName = "mickyn0415"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
